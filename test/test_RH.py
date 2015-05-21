5c3af80523dc065789adf30bdc6826a0fc81b3f7
djarecka <djarecka@gmail.com>
2015-05-20 18:55:29 -0600

    small editing

--- test/test_RH.py
+++ test/test_RH.py
@@ -5,9 +5,11 @@ import parcel as pc
 from scipy.io import netcdf
 import pytest
 
-#TODO: sprawdzic dla wiekszych z_max
+#TODO: sprawdzic dla wiekszych z_max  
+# wprowadzic epsilon (w sensie >= (1-eps))? 
+
 @pytest.mark.parametrize("kappa", [0, 0.5, 0.8])
-def test_RH_lt1(tmpdir, kappa):
+def test_RH_gt1(tmpdir, kappa):
     str_f = str(tmpdir.join("test_pcl.nc"))
     pc.parcel(outfile=str_f,  outfreq=1, kappa=kappa)
     f_out = netcdf.netcdf_file(str_f, "r")
