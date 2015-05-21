beab3489798c63b2daeff3bb8323e83c56c073ca
djarecka <djarecka@gmail.com>
2015-05-20 18:55:47 -0600

    adding other outfreq

--- test/test_smax.py
+++ test/test_smax.py
@@ -7,12 +7,16 @@ import os
 import pytest
 import pdb
 
-def test_smax(tmpdir):
+#checking if calculated RH_max is >= the largest value in a netcdf file  
+@pytest.mark.parametrize("outfreq", [1, 2, 20])
+def test_smax(tmpdir, outfreq):
     str_f = str(tmpdir.join("test_pcl.nc"))
     print str_f
-    pc.parcel(outfile=str_f,  outfreq=1)
+    pc.parcel(outfile=str_f,  outfreq=outfreq)
     f_out = netcdf.netcdf_file(str_f, "r")
-    assert f_out.variables["RH"][:].max() == f_out.RH_max
-
+    if outfreq == 1:
+        assert f_out.variables["RH"][:].max() == f_out.RH_max
+    else:
+        assert f_out.variables["RH"][:].max() <= f_out.RH_max
 
 
