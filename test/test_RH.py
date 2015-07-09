b244e746b5a8992062e5f3383a0657a68df27582
djarecka <djarecka@gmail.com>
2015-07-09 12:28:57 -0600

    changing test_RH,  kappa should be >0

--- test/test_RH.py
+++ test/test_RH.py
@@ -5,14 +5,10 @@ import parcel as pc
 from scipy.io import netcdf
 import pytest
 
-#TODO: sprawdzic dla wiekszych z_max  
-# wprowadzic epsilon (w sensie >= (1-eps))? 
-
-@pytest.mark.parametrize("kappa", [0, 0.5, 0.8])
+@pytest.mark.parametrize("kappa", [0.1, 0.5, 0.8])
 def test_RH_gt1(tmpdir, kappa):
     str_f = str(tmpdir.join("test_pcl.nc"))
     pc.parcel(outfile=str_f,  outfreq=1, kappa=kappa)
     f_out = netcdf.netcdf_file(str_f, "r")
     assert f_out.variables["RH"][-1] >= .99
 
-#TODO: dopisac test, ktory uwzglednia krzywizne itp. w porownaniu
