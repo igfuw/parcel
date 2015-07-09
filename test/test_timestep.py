5b68ce766a139b007a55935267bcbe0b4c44bea2
djarecka <djarecka@gmail.com>
2015-07-09 13:28:05 -0600

    testing options and adding diff to assert message,

--- test/test_timestep.py
+++ test/test_timestep.py
@@ -69,7 +69,7 @@ def test_timestep_diff(data, dt, eps=0.2):
     f_test = netcdf.netcdf_file(filename, "r")
     f_ref  = netcdf.netcdf_file(os.path.join("test/refdata", filename), "r")
     for var in f_ref.variables:
-         assert np.isclose(f_ref.variables[var][:], f_test.variables[var][:], atol=0, rtol=1.e-4).all(), "differs e.g. " + str(var) + "; absolute diff = " + str(abs(f_ref.variables[var][:] - f_test.variables[var][:]).max())   
+         assert np.isclose(f_ref.variables[var][:], f_test.variables[var][:], atol=0, rtol=1.e-8).all(), "differs e.g. " + str(var) + "; max(absolute diff) = " + str(abs(f_ref.variables[var][:] - f_test.variables[var][:]).max()) + "; max(absolute ref) = " + str(f_ref.variables[var][:].max())   
         
 
 def test_timestep_plot(data):
