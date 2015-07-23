0a50f44095f4ce34e28f4d73f1160617397b0713
trontrytel <ajaruga@igf.fuw.edu.pl>
2015-07-23 16:57:30 +0200

    wip on closed chem system + disoc test

--- sandbox/test_chem_closed_dsc.py
+++ sandbox/test_chem_closed_dsc.py
@@ -6,8 +6,8 @@ import numpy as np
 import math
 import subprocess
 
-from libcloudphxx import common as cm
 from parcel import parcel
+from libcloudphxx import common as cm
 
 def test_chem_closed():
     """
@@ -29,14 +29,15 @@ def test_chem_closed():
            SO2_g_0 = SO2_g_init, O3_g_0 = O3_g_init, H2O2_g_0 = H2O2_g_init,\
            chem_sys = 'closed',   outfile = outfile,\
            chem_dsl = True, chem_dsc = True, chem_rct = False,\
-           out_bin = ["chem:0/1/1/lin/wet/O3_a,H2O2_a,SO2_a,HSO3_a,SO3_a"],)
+           out_bin = '{"chem": {"rght": 1, "left": 0, "drwt": "wet", "nbin": 1, "lnli": "lin",\
+                       "moms": ["O3_a", "H2O2_a", "SO2_a", "HSO3_a", "SO3_a"]}}')
 
     f = netcdf.netcdf_file(outfile,   "r")
 
     Mass      = [cm.M_O3, cm.M_H2O2]
     chem_list = [   "O3",    "H2O2"]
     eps       = [  4e-11,      2e-4]
- 
+
     # convert aqueous phase within droplets to gase phase (mole fraction)
     def aq_to_g(chem_aq, rhod, T, M, p):
         return chem_aq * rhod * cm.R * T / M  / p
@@ -53,18 +54,41 @@ def test_chem_closed():
 
     # check for SO2_g -> SO2_a + HSO3- + SO3--
     ini = f.variables["SO2_g"][0] 
+    final_SO2_a = aq_to_g(f.variables["SO2_a"][-1],\
+                          f.variables["rhod"][-1], \
+                          f.variables["T"][-1], \
+                          cm.M_SO2,\
+                          f.variables["p"][-1])
+
+    final_HSO3_a = aq_to_g(np.squeeze(f.variables["chem_HSO3_a"][-1]),\
+                           f.variables["rhod"][-1],\
+                           f.variables["T"][-1],\
+                           cm.M_HSO3,\
+                           f.variables["p"][-1])
+
+    final_SO3_a = aq_to_g(np.squeeze(f.variables["chem_SO3_a"][-1]),\
+                          f.variables["rhod"][-1],\
+                          f.variables["T"][-1],\
+                          cm.M_SO3,\
+                          f.variables["p"][-1])
+
     #final gas phase = current gas phase + mass dissolved into droplets
-    end = f.variables["SO2_g"][-1] + \
-          aq_to_g(f.variables["SO2_a"][-1],  f.variables["rhod"][-1], f.variables["T"][-1], cm.M_SO2, f.variables["p"][-1]) + \
-          aq_to_g(np.squeeze(f.variables["chem_HSO3_a"][-1]), f.variables["rhod"][-1], f.variables["T"][-1], cm.M_HSO3, f.variables["p"][-1]) + \
-          aq_to_g(np.squeeze(f.variables["chem_SO3_a"][-1]),  f.variables["rhod"][-1], f.variables["T"][-1], cm.M_SO3, f.variables["p"][-1])
+    end = f.variables["SO2_g"][-1] + final_SO2_a + final_HSO3_a + final_SO3_a 
 
     print "  "
-    print ini
-    print end
-    print (ini - end) / ini
+    print "M SO2 = " , cm.M_SO2
+    print "M HSO3 = ", cm.M_HSO3
+    print "M SO3 = " , cm.M_SO3
+    print " "
+    print "initial SO2 mole frac =   ", ini
+    print "final SO2 mole frac =     ", f.variables["SO2_g"][-1]
+    print "mole frac due to SO2_a =  ", final_SO2_a
+    print "mole frac due to HSO3_a = ", final_HSO3_a
+    print "mole frac due to SO3_a =  ", final_SO3_a
+    print "sum at the end =          ", end
+    print "sum - ini / ini =         ", (ini - end) / ini
 
     assert np.isclose(end, ini, atol=0, rtol=4e-9)
    
     # cleanup
-    #subprocess.call(["rm", outfile])
+    subprocess.call(["rm", outfile])
