f9ff976c19973f2af2736369abd7d896ad6a804e
trontrytel <ajaruga@igf.fuw.edu.pl>
2015-07-24 13:39:53 +0200

    cleanup in test_chem_closed_dsl

--- unit_test/test_chem_closed_dsl.py
+++ unit_test/test_chem_closed_dsl.py
@@ -5,16 +5,13 @@ from scipy.io import netcdf
 import numpy as np
 import math
 import subprocess
+import pytest
 
-from libcloudphxx import common as cm
 from parcel import parcel
+from libcloudphxx import common as cm
 
-def test_chem_closed():
-    """
-    Checking if the total mass of SO_2, O_3 and H2O2 in the closed chemical system 
-    with only dissocoation present remains constant
-
-    """
+@pytest.fixture(scope="module")
+def data(request):
     #TODO - the same for dissociation
     #TODO - think of a similar test for reactions
     SO2_g_init  =  200e-12
@@ -30,27 +27,35 @@ def test_chem_closed():
            SO2_g_0 = SO2_g_init, O3_g_0 = O3_g_init, H2O2_g_0 = H2O2_g_init,\
            chem_sys = 'closed',   outfile = outfile,\
            chem_dsl = True, chem_dsc = False, chem_rct = False,\
-           out_bin = '{"chem": {"rght": 1, "moms": ["O3_a", "H2O2_a", "SO2_a"], "drwt": "wet", "nbin": 1, "lnli": "lin", "left": 0}}',)
+           out_bin = '{"chem":\
+                        {"rght": 1, "left": 0, "drwt": "wet", "lnli": "lin", "nbin": 1, "moms": ["O3_a", "H2O2_a", "SO2_a"]}}')
 
-    f = netcdf.netcdf_file(outfile,   "r")
+    data = netcdf.netcdf_file(outfile,   "r")
 
-    Mass      = [cm.M_SO2, cm.M_O3, cm.M_H2O2]
-    chem_list = ["SO2",       "O3",    "H2O2"]
-    eps       = [4e-9,       4e-11,      2e-4]
-                                        #TODO why so big?
+    # removing all netcdf files after all tests                                      
+    def removing_files():
+        subprocess.call(["rm", outfile])
+
+    request.addfinalizer(removing_files)
+    return data
+
+@pytest.mark.parametrize("chem", ["SO2", "O3", "H2O2"])                   #TODO why so big?
+def test_chem_closed_dsl(data, chem, eps = {"SO2": 4e-9, "O3": 4e-11, "H2O2": 2e-4}):
+    """
+    Checking if the total mass of SO_2, O_3 and H2O2 in the closed chemical system 
+    with only dissocoation present remains constant
+
+    """
+    molar_mass = getattr(cm, "M_"+chem)
 
     # convert aqueous phase within droplets to gase phase (mole fraction)
     def aq_to_g(chem_aq, rhod, T, M, p):
         return chem_aq * rhod * cm.R * T / M  / p
 
-    for chem in chem_list : 
-        ini = f.variables[chem+"_g"][0] 
-        #final gas phase = current gas phase + mass dissolved into droplets
-        end = f.variables[chem+"_g"][-1] + \
-              aq_to_g(f.variables[chem+"_a"][-1], f.variables["rhod"][-1], f.variables["T"][-1],\
-                    Mass[chem_list.index(chem)], f.variables["p"][-1])
+    ini = data.variables[chem+"_g"][0] 
+    #final gas phase = current gas phase + mass dissolved into droplets
+    end = data.variables[chem+"_g"][-1] + \
+          aq_to_g(data.variables[chem+"_a"][-1], data.variables["rhod"][-1], data.variables["T"][-1],\
+                molar_mass, data.variables["p"][-1])
 
-        assert np.isclose(end, ini, atol=0, rtol=eps[chem_list.index(chem)])
-    
-    # cleanup
-    subprocess.call(["rm", outfile])
+    assert np.isclose(end, ini, atol=0, rtol=eps[chem])
