6266ef6661de0d0b1730b3c16c97eb90103959a7
trontrytel <ajaruga@igf.fuw.edu.pl>
2015-11-12 17:45:13 +0100

    wip on initial dry distribution test

--- unit_test/test_mass.py
+++ unit_test/test_mass.py
@@ -27,10 +27,18 @@ def data(request):
     p_init  = 95000.
     r_init  = rh_to_rv(RH_init, T_init, p_init)
 
+    epp = cm.R_d / cm.R_v
+    e_init = p_init * r_init / (r_init + epp)
+
+    p_stc = 101325
+    T_stc = 273.15 + 15
+ 
+    n_tot = 566.e6 * T_init / T_stc * p_stc / (p_init - e_init)
+
     # running parcel model for open / closed chem system  ...
     parcel(dt = .1, sd_conc = 1024, outfreq = 40, outfile = outfile, z_max = 600., w = .5,\
            T_0 = T_init, p_0 = p_init, r_0 = r_init, \
-           chem_rho = 1.8e3, mean_r = .04e-6, gstdev = 2., n_tot = 566.e6, out_bin = \
+           chem_rho = 1.8e3, mean_r = .04e-6, gstdev = 2., n_tot = n_tot, out_bin = \
             '{"wradii": {"rght": 1e-4, "left": 1e-10, "drwt": "wet", "lnli": "lin", "nbin": 500, "moms": [0, 3]}, \
               "dradii": {"rght": 1e-4, "left": 1e-10, "drwt": "dry", "lnli": "lin", "nbin": 500, "moms": [0, 3]}}'
           )
@@ -44,7 +52,7 @@ def data(request):
     #request.addfinalizer(removing_files)
     return data
 
-def test_water_const(data, eps = 6e-15):
+def test_water_const(data, eps = 7e-15):
     """
     Check if the total water is preserved
 
@@ -71,43 +79,34 @@ def test_dry_mass_const(data, eps = 1e-20):
     """
     chem_rho = getattr(data, "chem_rho")
     mom3     = data.variables["dradii_m3"][:,:]
-    #rhod     = data.variables["rhod"][:]
-    #rv       = data.variables["r_v"][:]
+    rhod     = data.variables["rhod"][:]
+    rv       = data.variables["r_v"][:]
 
     ini = mom3[0,:].sum()  * 4./3 * math.pi * chem_rho
     end = mom3[-1,:].sum() * 4./3 * math.pi * chem_rho
 
     assert np.isclose(end, ini, atol=0, rtol=eps), str((ini-end)/ini)
 
-#    epp = cm.R_d / cm.R_v
-#    rhod_parc_init = rhod[0]
-#    rho_parc_init  = rhod[0] * (rv[0] * rv[0] + 1.) / (rv[0] + 1.)
-#    rhod_art_init  = 95000 * epp / (epp + rv[0]) / 285.2 / cm.R_d
-#    rho_art_init   = rhod_art_init * (rv[0] * rv[0] + 1.) / (rv[0] + 1.)
-#
-#    a = 2.375 * 1e-9 / rhod_parc_init
-#    b = 2.375 * 1e-9 / rho_parc_init
-#    c = 2.375 * 1e-9 / rhod_art_init
-#    d = 2.375 * 1e-9 / rho_art_init
-#
-#    print " "
-#    print " "
-#
-#    print "rhod_parc_init ", rhod_parc_init
-#    print "rhod_art_init  ", rhod_art_init
-#    print "rho_parc_init  ", rho_parc_init
-#    print "rho_art_init   ", rho_art_init
-#    print " "
-#
-#
-#    print "initial mixing ratio: ", ini
-#    print "final mixing ratio:   ", end
-#    print " "
-#
-#
-#    print "article init mixing ratio (/ rhod parc init):  ", a
-#    print "article init mixing ratio (/ rhod art init):   ", c
-#    print "article init mixing ratio (/ rho parc init):   ", b
-#    print "article init mixing ratio (/ rho art init):    ", d
-#    print " "
-#
+    epp = cm.R_d / cm.R_v
+    rhod_parc_init = rhod[0]
+    rho_parc_init  = rhod[0] * (rv[0] * rv[0] + 1.) / (rv[0] + 1.)
+    rhod_art_init  = 95000 * epp / (epp + rv[0]) / 285.2 / cm.R_d
+    rho_art_init   = rhod_art_init * (rv[0] * rv[0] + 1.) / (rv[0] + 1.)
+
+    a = 2.375 * 1e-9 / rhod_parc_init
+    b = 2.375 * 1e-9 / rho_parc_init
+    c = 2.375 * 1e-9 / rhod_art_init
+    d = 2.375 * 1e-9 / rho_art_init
+
+    print " "
+    print " "
+
+    print "initial mixing ratio: ", ini
+    print "final mixing ratio:   ", end
+    print " "
+
+    print "article init mixing ratio (/ rhod parc init):  ", a
+    print "article init mixing ratio (/ rhod art init):   ", c
+    print "article init mixing ratio (/ rho parc init):   ", b
+    print "article init mixing ratio (/ rho art init):    ", d
+    print " "
